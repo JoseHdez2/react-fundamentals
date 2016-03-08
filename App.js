eeebb3dd1769592ed06d982a9a60ecc84c56f370
Jose Hernandez <josemanuelhdez2@gmail.com>
2016-03-08 21:07:37 +0000

    03 React Fundamentals
    The Render Method

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-// class App extends React.Component {
-//      render(){
-//           return <h1>Hello Guys</h1>
-//
-//      }
-// }
-
-const App = () => <h1>Hello Eggheads</h1>
+class App extends React.Component {
+     render(){
+        return (
+          <div>
+            <h1>Hello World</h1>
+            <b>Bold</b>
+          </div>
+        );
+     }
+}
 
 export default App
