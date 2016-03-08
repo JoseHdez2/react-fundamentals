7cd429a8c7614eda276949fa1e909c4ad497157f
Jose Hernandez <josemanuelhdez2@gmail.com>
2016-03-08 22:20:39 +0000

    06 React Fundamentals
    Owner Ownee Relationship

--- App.js
+++ App.js
@@ -4,10 +4,8 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {
-      txt: 'this is the state txt',
-      cat: 0
-    }
+    this.state = { txt: '' }
+    this.update = this.update.bind(this)
   }
 
   update(e){
@@ -17,11 +15,22 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <input type="text" name="" id="" onChange={this.update.bind(this)}/>
-        <h1>{this.state.txt}</h1>
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
       </div>
-    )
+    );
   }
+}
+
+const Widget = (props) => {
+    return (
+      <div>
+        <input type="text" onChange={props.update}/>
+        <h1>{props.txt}</h1>
+      </div>
+    );
 }
 
 export default App
